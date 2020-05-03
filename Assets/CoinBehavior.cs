f11cf2ad33c914e388e2cdcc336762cb815d5379
niole <niolenelson@gmail.com>
2020-05-03 15:10:25 -0700

    Destroy coins if you come into contact with them

---
+++ Assets/CoinBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehavior : MonoBehaviour
+{
+
+    bool isDestroyed = false;
+
+    int totalFlickers = 0;
+
+    int maxFlixers = 30;
+
+    Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (isDestroyed)
+        {
+            totalFlickers += 1;
+            if (totalFlickers < maxFlixers && totalFlickers%2 == 0)
+            {
+                renderer.enabled = !renderer.enabled;
+            }
+
+            if (totalFlickers == maxFlixers)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isDestroyed = true;
+        }
+    }
+}
