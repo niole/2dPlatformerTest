fe462322c384f0b68aaad5942283b42211546c19
niole <niolenelson@gmail.com>
2020-05-10 11:20:51 -0700

    Refactor basic terrain generation to be 'pageable'

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -7,96 +7,194 @@ using System.Linq;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    public int softUpperBound = 60; // can go over this bound in on of the iterations, but then that's enough
+    static public int softUpperBound = 60; // can go over this bound in on of the iterations, but then that's enough
 
     public int maxHeight = 4;
 
     public Tilemap tileMap;
 
-    public TileBase horizontalTileStart;
+    public TileBase horizontalTileStart; // 1
 
-    public TileBase horizontalTileMid;
+    public TileBase horizontalTileMid; // 2
 
-    public TileBase horizontalTileEnd;
+    public TileBase horizontalTileEnd; // 3
 
-    public TileBase slantedDownTileStart;
+    public TileBase slantedDownTileStart; // 4
 
-    public TileBase slantedDownTileEnd;
+    public TileBase slantedDownTileEnd; // 5
 
-    public TileBase slantedUpTileStart;
+    public TileBase slantedUpTileStart; // 6
 
-    public TileBase slantedUpTileEnd;
+    public TileBase slantedUpTileEnd; // 7
 
-    public TileBase dirt;
+    public TileBase dirt; // 8
 
     public Rigidbody2D coin;
 
-    int xStart = -2;
+    Dictionary<int, TileBase> tileBaseTable = new Dictionary<int, TileBase>();
+
+    static float fetchError = 0.25f;
+
+    static int xAbsoluteStart = -2;
+
+    int xStart = xAbsoluteStart;
 
     int yStart = -3;
+
+    float sectionStartIndex = 0f;
+
+    float sectionEndIndex = 0f;
+
+    float shouldFetchRightOffset = (1f - fetchError) * softUpperBound;
+
+    float shouldFetchLeftOffset = fetchError * softUpperBound;
+
+    List<List<List<int>>> state;
 
     // TODO when user goes far enough right, we generate more and delete the oldest, vice versa
     // if user wants to go backwards, they will get the same terrain that was originally there
     // if a user has collected the coins already, they will not be there
+    //
+    // need to store all terrain representation, includes no/coin
+    // have multidimentional array, x offset is the index of main array, subarray is  y, has tile, tile type, has coin
+    // [ [ [yval, tile type, has coin], [], [] ], ...]
+    // [ [
+    //
+    // need "active section" endpoints
+    //
+    // need "get more right" offset
+    // need "get more left" offset
+    // TODO when collide with coin need to update coin state
+
+    bool ShouldGetRight(float playerX)
+    {
+        float playerOffset = playerX - xStart;
+        // always allow right fetching when possible
+        // TODO create absolute upper bound eventually
+        return playerOffset >= shouldFetchRightOffset;
+    }
+
+    bool ShouldGetLeft(float playerX)
+    {
+        float playerOffset = playerX - xStart;
+
+        // only fetch left if not at start
+        return playerOffset <= shouldFetchLeftOffset && playerX > xAbsoluteStart + shouldFetchLeftOffset;
+    }
+
+    void MaybeUpdateActiveSection(float playerX)
+    {
+        if (ShouldGetLeft(playerX))
+        {
+            // fetch left, garbage collect right
+        } else if (ShouldGetRight(playerX))
+        {
+            // fetch right, garbage collect left
+        }
+    }
 
     // 0 flat
     // 1 up one
     // 2 up two
     void Start()
     {
-        PlayerMoveEvents.current.onPlayerMoveTriggerEnter += OnPlayerMove;
+        tileBaseTable.Add(1, horizontalTileStart);
+        tileBaseTable.Add(2, horizontalTileMid);
+        tileBaseTable.Add(3, horizontalTileEnd);
+        tileBaseTable.Add(4, slantedDownTileStart);
+        tileBaseTable.Add(5, slantedDownTileEnd);
+        tileBaseTable.Add(6, slantedUpTileStart);
+        tileBaseTable.Add(7, slantedUpTileEnd);
+        tileBaseTable.Add(8, dirt);
 
         tileMap = GetComponent<Tilemap>();
-        int level = 0;
-        int yOffset = 0;
-        int xOffset = 0;
+        PlayerMoveEvents.current.onPlayerMoveTriggerEnter += OnPlayerMove;
+        state = BuildState(0);
+        RenderState();
+    }
 
-        while (xOffset < softUpperBound)
+    // TODO assumes everything previously (in space) has been built correctly
+    // there is no dependency on what was previously built
+    List<List<List<int>>> BuildState(int initialLevel)
+    {
+        List<int> levelChoices = new List<int>{ -1, 0, 1 };
+
+        int level = initialLevel;
+
+        List<List<List<int>>> nextState = new List<List<List<int>>>(softUpperBound);
+
+        for (int i = 0; i < softUpperBound; i++)
         {
-            List<int> levelChoices = new List<int>{ -1, 0, 1 };
             var choices = levelChoices.Where(l => level + l >= 0 && level + l <= maxHeight).ToList();
             int nextChoiceIndex = Convert.ToInt32(UnityEngine.Random.Range(0f, Convert.ToSingle(choices.Count() - 1)));
-            bool shouldRenderCoin = false;
 
             int choice = choices[nextChoiceIndex];
 
             level += choice;
-            xOffset += 1;
 
-            if (choice < 0)
+            if (choice == -1)
             {
-                // going down
-                Vector3Int firstTilePos = new Vector3Int(xStart + xOffset, yStart + yOffset, 0);
-                tileMap.SetTile(firstTilePos, slantedDownTileStart);
-                Vector3Int secondTilePos = new Vector3Int(xStart + xOffset, yStart + yOffset - 1, 0);
-                tileMap.SetTile(secondTilePos, slantedDownTileEnd);
-
-                yOffset -= 1;
+                nextState.Add(new List<List<int>> {
+                    new List<int> {level + 1, 4, 0},
+                    new List<int> {level, 5, 0}
+                });
             } else if (choice == 0)
             {
-                shouldRenderCoin = UnityEngine.Random.Range(0f, 100f) <= 20f;
+                bool shouldRenderCoin = UnityEngine.Random.Range(0f, 100f) <= 20f;
+                if (shouldRenderCoin)
+                {
+                    nextState.Add(new List<List<int>> {
+                        new List<int> {level, 2, 1}
+                    });
+                } else
+                {
+                    nextState.Add(new List<List<int>> {
+                        new List<int> {level, 2, 0}
+                    });
+                }
 
-                // staying level, y doesn't change
-                Vector3Int tilePos = new Vector3Int(xStart + xOffset, yStart + yOffset, 0);
-                tileMap.SetTile(tilePos, horizontalTileMid);
-            } else if (choice > 0)
+            } else if (choice == 1)
             {
-                // going up, y goes up
-                Vector3Int firstTilePos = new Vector3Int(xStart + xOffset, yStart + yOffset, 0);
-                tileMap.SetTile(firstTilePos, slantedUpTileStart);
-                Vector3Int secondTilePos = new Vector3Int(xStart + xOffset, yStart + yOffset + 1, 0);
-                tileMap.SetTile(secondTilePos, slantedUpTileEnd);
-
-                yOffset += 1;
-            }
-
-            if (shouldRenderCoin)
-            {
-                Instantiate(coin, new Vector3(xOffset + xStart, yOffset + yStart + 3, 0), Quaternion.identity);
+                nextState.Add(new List<List<int>> {
+                    new List<int> {level - 1, 6, 0},
+                    new List<int> {level, 7, 0}
+                });
             }
         }
+
+        return nextState;
     }
 
+    void RenderState()
+    {
+        List<List<List<int>>> stateToRender = state;
+
+        for (int xOffset = 0; xOffset < softUpperBound; xOffset++)
+        {
+            List<List<int>> renderableXSection = stateToRender[xOffset];
+            for (int j = 0; j < renderableXSection.Count(); j++)
+            {
+                List<int> yDetails = renderableXSection[j];
+                int yOffset = yDetails[0];
+                int tileType = yDetails[1];
+                int hasCoin = yDetails[2];
+
+                Vector3Int tilePos = new Vector3Int(xStart + xOffset, yStart + yOffset, 0);
+                TileBase tile = tileBaseTable[tileType];
+                if (tile != null)
+                {
+                    tileMap.SetTile(tilePos, tile);
+                }
+
+                if (hasCoin == 1)
+                {
+                    Instantiate(coin, new Vector3(xOffset + xStart, yOffset + yStart + 3, 0), Quaternion.identity);
+                }
+
+            }
+        }
+
+    }
 
     private void OnPlayerMove(float xLocation)
     {
